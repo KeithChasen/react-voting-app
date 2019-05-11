cd1ebedbad62b74221f41bf90ad7c18c4a0c363a
keithchasen <keithchasen89@gmail.com>
2019-05-11 11:59:08 +0300

    product list component / semanti ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import ProductList from './components/ProductList'
+import 'semantic-ui-css/semantic.min.css'
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="App main ui text container">
+        <h1 className="ui dividing centered header">Popular Products</h1>
+        <div id="content">
+            <ProductList />
+        </div>
     </div>
   );
 }
---
+++ src/components/ProductList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class ProductList extends Component {
+    render() {
+        return (
+            <div className="ui unstackable items">
+                React Component
+            </div>
+        )
+    }
+}
+
+export default ProductList
